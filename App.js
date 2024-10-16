64915c615b6e2cc521cf116f6fa88963aabd7496
Cilven <Minhtqce170364@fpt.edu.vn>
2024-10-16 18:35:25 +0700

    Add files via upload

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Profile from "./component/Profile";
+import EditProfile from "./component/EditProfile";
+import NotificationScreen from "./component/notification";
+import MessageScreen from "./component/Message";
+import ProjectComments from "./component/ProfileComment";
+import StoreRecordsScreen from "./component/StoreRecordsScreen";
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Profile">
+        <Stack.Screen name="Profile" component={Profile} />
+        <Stack.Screen name="EditProfile" component={EditProfile} />
+      </Stack.Navigator>
+    </NavigationContainer>
+    // <NavigationContainer>
+    //   <Stack.Navigator initialRouteName="Notifications">
+    //     <Stack.Screen name="Notifications" component={NotificationScreen} />
+    //     <Stack.Screen name="Message" component={MessageScreen} />
+    //   </Stack.Navigator>
+    // </NavigationContainer>
+    // <NavigationContainer>
+    //   <Stack.Navigator initialRouteName="ProjectComments">
+    //     <Stack.Screen
+    //       name="ProjectComments"
+    //       component={ProjectComments}
+    //       options={{ headerShown: false }} // Hide the header
+    //     />
+    //     <Stack.Screen
+    //       name="StoreRecords"
+    //       component={StoreRecordsScreen}
+    //       options={{ headerShown: false }} // Hide the header if needed on this screen too
+    //     />
+    //   </Stack.Navigator>
+    // </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
